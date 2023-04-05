ceff64d56768c14fd1f3421704d049687ccd8d2f
Mutindi Hope <msndindi@outlook.com>
2023-04-05 14:28:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+
+ 
+
 // Code your solution in this file!
+
+// const drivers = ;
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers,
+];
+
+
+function createFareMultiplier(integer) {
+  return function (fare) {
+    return fare * integer;
+  };
+}
+
+const fareDoubler = function (fare) {
+    return fare * 2
+}
+const fareTripler = function (fare) {
+  return fare * 3;
+};
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+
+    return returnFirstTwoDrivers(drivers);
+    
+}
+
